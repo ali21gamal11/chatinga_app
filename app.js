b97bed89fda62c11fa6a38a7288875b061e9b410
ali gamal <ali909090k@gmail.com>
2025-10-11 22:07:06 +0300

    fix register

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors =require("cors");
 const connect = require('./config/connect');
 connect();
 
+const logger = require('./middleware/logger');
 const user = require('./routes/userRoutes');
 const message = require('./routes/messageRoutes');
+const auth = require('./routes/auth');
 
+app.use(logger)
 app.use(express.json());
-
+app.use(cors());
 app.use('/api/user',user)
 app.use('/api/message',message);
+app.use('/api/auth',auth);
 
 const port = process.env.PORT
 app.listen(port,()=>{
